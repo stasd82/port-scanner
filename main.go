29c2b85ff450a9ab2102cec5f3fcdfc008221eb6
sta <stasd82@yandex.ru>
2022-06-20 20:36:40 +0300

    Improve with Worker Pool

--- main.go
+++ main.go
@@ -9,28 +9,36 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
+	portsChan := make(chan int, 10)
 
-	for i := 1; i < 1024; i++ {
-		wg.Add(1)
-
-		go func(portNumber int) {
-			defer wg.Done()
-
-			if portNumber == 25 {
-				return
-			}
-
-			dest := "scanme.nmap.org:" + strconv.Itoa(portNumber)
-			conn, err := net.Dial("tcp", dest)
-
-			if err != nil {
-				return
-			}
-
-			conn.Close()
-			fmt.Printf("~> got conn for %v\n", dest)
-		}(i)
+	for i := 0; i < cap(portsChan); i++ {
+		go worker(portsChan, &wg)
 	}
+
+	for i := 1; i <= 24; i++ {
+		wg.Add(1)
+		portsChan <- i
+	}
+
+	close(portsChan)
 	wg.Wait()
+
 	fmt.Println("done!")
+}
+
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for p := range ports {
+		dest := "scanme.nmap.org:" + strconv.Itoa(p)
+		conn, err := net.Dial("tcp", dest)
+
+		if conn != nil {
+			fmt.Printf("~> got conn for %v\n", dest)
+			conn.Close()
+		}
+		if err != nil {
+			fmt.Printf("~> error for %v\n", dest)
+		}
+
+		wg.Done()
+	}
 }
